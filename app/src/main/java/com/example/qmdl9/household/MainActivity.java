d47d0ecb401e1e622d8a2eae40c2c2b8c700df37
qmdl980 <qmdl980@naver.com>
2017-06-12 09:15:45 +0900

    입출금 기록을 제일 최근것만 출력해주는 시스템

--- app/src/main/java/com/example/qmdl9/household/MainActivity.java
+++ app/src/main/java/com/example/qmdl9/household/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
     int balance;
+    String init;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +16,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         balance = 100;
         TextView Balance = (TextView) findViewById(R.id.balance);
+        TextView Record = (TextView) findViewById(R.id.init);
         Balance.setText(Integer.toString(balance));
+        Record.setText(init);
     }
 
     public void Deposit(View v){
         EditText deposit_amount = (EditText) findViewById(R.id.deposit_amount);
+        EditText get_money = (EditText) findViewById(R.id.get_money);
+        init = get_money.toString();
         int amount = Integer.parseInt(deposit_amount.getText().toString());
         balance += amount;
         TextView Balance = (TextView) findViewById(R.id.balance);
+        TextView Record = (TextView) findViewById(R.id.init);
         Balance.setText(Integer.toString(balance));
-        /*Intent intent = new Intent(getApplicationContext(), SubActivity.class);
-        startActivity(intent);*/
+        Record.setText(init);
     }
     public void Debit(View v){
         EditText debit_amount = (EditText) findViewById(R.id.debit_amount);
+        EditText use_money = (EditText) findViewById(R.id.use_money);
+        init = use_money.toString();
         int amount = Integer.parseInt(debit_amount.getText().toString());
         balance -= amount;
         TextView Balance = (TextView) findViewById(R.id.balance);
+        TextView Record = (TextView) findViewById(R.id.init);
         Balance.setText(Integer.toString(balance));
-        /*Intent intent = new Intent(getApplicationContext(), SubActivity.class);
-        startActivity(intent);*/
+        Record.setText(init);
     }
 }
